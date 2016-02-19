f44febb92423aab7ab80e44dbc3cb180e7db9d2c
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-02-19 09:55:15 -0500

    changed wsgi.py file

--- social_todo/wsgi.py
+++ social_todo/wsgi.py
@@ -15,6 +15,6 @@ from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_todo.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_todo.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
