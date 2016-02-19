e8a7442eb77848abe01bcd1d451ec22a53cb3a6d
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-02-18 21:52:17 -0500

    updated requirements

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn myproject.wsgi --log-file -
+web: gunicorn social-todo-2.wsgi --log-file -
--- social_todo/settings.py
+++ social_todo/settings.py
@@ -41,7 +41,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
     },
     'OPTIONS': {
         'isolation_level': psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE,
