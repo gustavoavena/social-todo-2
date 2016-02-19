1ac9bb2bf3059a45ac9a3bff0ca7c31f9f49d756
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-02-18 22:39:12 -0500

    blank commit again to restart heroku

--- social_todo/settings.py
+++ social_todo/settings.py
@@ -58,6 +58,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
