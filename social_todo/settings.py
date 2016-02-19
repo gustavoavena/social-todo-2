c54a607341b6bc01bc3cf63f7c1ecee569755614
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-02-18 21:45:02 -0500

    updated requirements

--- social_todo/settings.py
+++ social_todo/settings.py
@@ -85,6 +85,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.db.backends.postgresql'
 ]
 
 MIDDLEWARE_CLASSES = [
