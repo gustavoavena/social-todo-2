bc3c8cd2a8f251354e4d51bc8d117fe95e04a393
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-02-19 11:38:47 -0500

    fixed a typo on the tasks form.py file

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,7 +6,7 @@ MAX_LENGTH = 50
 class CreateTaskForm(forms.Form):
     title = forms.CharField(label='Title', max_length=500, widget=forms.TextInput(attrs={'placeholder': 'Title'}))
     description = forms.CharField(label="Description", max_length=5000, widget=forms.TextInput(attrs={'placeholder': 'Description'}))
-    collaborator1 = forms.CharField(max_length=MAX_LE2NGTH, required=False, widget=forms.TextInput(attrs={'placeholder': 'Collaborator 1'}))
+    collaborator1 = forms.CharField(max_length=MAX_LENGTH, required=False, widget=forms.TextInput(attrs={'placeholder': 'Collaborator 1'}))
     collaborator2 = forms.CharField(max_length=MAX_LENGTH, required=False, widget=forms.TextInput(attrs={'placeholder': 'Collaborator 2'}))
     collaborator3 = forms.CharField(max_length=MAX_LENGTH, required=False, widget=forms.TextInput(attrs={'placeholder': 'Collaborator 3'}))
     
